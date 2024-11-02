7c63a90f56e0f5f16d99652a7f7506ecd71d6172
pedro <pedro.jcaceresl@gmail.com>
2024-11-02 15:52:59 -0300

    feat: add the note model

---
+++ src/main/java/com/crud/basic/back/Model/Note.java
@@ -0,0 +1,33 @@
+package com.crud.basic.back.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+}
